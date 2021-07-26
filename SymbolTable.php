0379dc453f7812364fe78382656b93c7b7109a08
suzuken <suzuken326@gmail.com>
2021-07-26 14:07:12 +0900

    add test

--- SymbolTable.php
+++ SymbolTable.php
@@ -4,92 +4,8 @@ declare(strict_types=1);
 
 namespace Ion;
 
-class SharedSymbolTable extends SymbolTable
+class SymbolTable
 {
-    private string $name;
-    private int $version;
-    private array $symbols;
-    /**
-     * @var array
-     */
-    private array $index;
-    private int $maxId;
-
-    /**
-     * SharedSymbolTable constructor.
-     * @param string $name
-     * @param int $version
-     * @param string[] $symbols
-     */
-    public function __construct(string $name, int $version, array $symbols)
-    {
-        $this->name = $name;
-        $this->version = $version;
-        $this->symbols = $symbols;
-        $this->index = $this->buildIndex($symbols, 1);
-        $this->maxId = count(syms);
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getVersion(): int
-    {
-        return $this->version;
-    }
-
-    public function getSymbols(): array
-    {
-        return $this->symbols;
-    }
-
-    public function getIndex(): array
-    {
-        return $this->index;
-    }
-
-    public function getMaxId(): int
-    {
-        return $this->maxId;
-    }
-
-    private function buildIndex($symbols, int $offset): array
-    {
-        $index = [];
-        foreach ($symbols as $i => $sym) {
-            if ($sym !== "") {
-                if (!isset($index[$sym])) {
-                    $index[$sym] = $offset + $i;
-                }
-            }
-        }
-        return $index;
-    }
-
-    public function find(string $symbol): ?SymbolToken
-    {
-        $id = $this->findByName($symbol);
-        if ($id === null) {
-            return null;
-        }
-        $text = $this->findById($id);
-        if ($text === null) {
-            return null;
-        }
-        return new SymbolToken($text, $id, nil);
-    }
-
-    public function findByName(string $symbol): ?int
-    {
-        return $this->index[$symbol] ?? null;
-    }
-
-    public function findById(int $id): ?string
-    {
-        return $this->symbols[$id-1] ?? null;
-    }
 }
 
 class ImportSource
@@ -110,8 +26,4 @@ class SymbolToken
         $this->localSID = $localSID;
         $this->importSource = $importSource;
     }
-}
-
-class SymbolTable
-{
 }
--- Tests/SharedSymbolTableTest.php
+++ Tests/SharedSymbolTableTest.php
@@ -1,12 +1,23 @@
 <?php
 
+declare(strict_types=1);
 
+use Ion\SharedSymbolTable;
 use PHPUnit\Framework\TestCase;
 
 class SharedSymbolTableTest extends TestCase
 {
     public function testName()
     {
+        $st = new SharedSymbolTable('test', 2, [
+            'abc',
+            'def',
+            'def',
+            'ghi',
+        ]);
 
+        $this->assertSame('test', $st->getName());
+        $this->assertSame(2, $st->getVersion());
+        $this->assertSame(4, $st->getMaxId());
     }
 }
